ff2842832e446ee8ca6c2e806930ddf51025d099
Gaz Lock <gazlockwood@gmail.com>
2020-05-11 19:22:42 +0100

    + IIS - tomcat 7.0.88 functionallity, + Locale setting script to en-GB, + Fixed logic in ATA install script, + Fixed install-redteam.ps1 error, + Updated VagrantFile to add VMs to groups in virtualbox

--- Vagrant/scripts/install-microsoft-ata.ps1
+++ Vagrant/scripts/install-microsoft-ata.ps1
@@ -1,6 +1,6 @@
 # Purpose: Downloads, installs and configures Microsft ATA 1.9
 $title = "Microsoft ATA 1.9"
-$downloadUrl = "http://download.microsoft.com/download/4/9/1/491394D1-3F28-4261-ABC6-C836A301290E/ATA1.9.iso"
+$downloadUrl = "https://download.microsoft.com/download/4/9/1/491394D1-3F28-4261-ABC6-C836A301290E/ATA1.9.iso"
 $fileHash = "DC1070A9E8F84E75198A920A2E00DDC3CA8D12745AF64F6B161892D9F3975857" # Use Get-FileHash on a correct downloaded file to get the hash
 
 # Enable web requests to endpoints with invalid SSL certs (like self-signed certs)
@@ -50,14 +50,15 @@ if (-not (Test-Path "C:\Program Files\Microsoft Advanced Threat Analytics\Center
         If (-not ($actualHash -eq $fileHash))
         {
             Write-Host "$title.iso was not downloaded correctly: hash from downloaded file: $actualHash, should've been: $fileHash. Re-trying using BitsAdmin now..."
+            Remove-Item -Path "$env:temp\$title.iso" -Force
+            bitsadmin /Transfer ATA $downloadUrl "$env:temp\$title.iso"
+            $actualHash = (Get-FileHash -Algorithm SHA256 -Path "$env:temp\$title.iso").Hash
+            If (-not ($actualHash -eq $fileHash))
+            {
+                throw "$title.iso was not downloaded correctly after a retry: hash from downloaded file: $actualHash, should've been: $fileHash - Giving up."
+            }
         }
-        Remove-Item -Path "$env:temp\$title.iso" -Force
-        bitsadmin /Transfer ATA $downloadUrl "$env:temp\$title.iso"
-        $actualHash = (Get-FileHash -Algorithm SHA256 -Path "$env:temp\$title.iso").Hash
-        If (-not ($actualHash -eq $fileHash))
-        {
-            throw "$title.iso was not downloaded correctly after a retry: hash from downloaded file: $actualHash, should've been: $fileHash - Giving up."
-        }
+       
     }
     $Mount = Mount-DiskImage -ImagePath "$env:temp\$title.iso" -StorageType ISO -Access ReadOnly -PassThru
     $Volume = $Mount | Get-Volume
--- Vagrant/scripts/install-tomcat.ps1
+++ Vagrant/scripts/install-tomcat.ps1
@@ -1,4 +1,48 @@
-﻿#Install Tomcat and jdk, this is for iis server only
+﻿#Pupose: Install Tomcat and jdk, this is for iis server only
 
-choco install jdk8
-choco install tomcat --version=7.0.69
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing additional Choco packages..."
+
+If (-not (Test-Path "C:\ProgramData\chocolatey")) {
+  Write-Host "Installing Chocolatey"
+  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+  Write-Host "Chocolatey is already installed."
+}
+
+Write-Host "Installing Tomcat with requirements..."
+choco install -y --limit-output --no-progress jdk8
+
+#Set up variables
+$title = "apache-tomcat-7.0.88"
+$downloadUrl = "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.88/bin/apache-tomcat-7.0.88-windows-x64.zip"
+$filehash = "0d1151e15599802fcc0e71e62d8a3bba3644d1e3"
+$location = "$env:SystemDrive\$title"
+$env:CATALINA_HOME = $location
+$env:CATALINA_BASE = $location
+
+#Allow SSL traffic with poor security (ish)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Download Tomcat 7.0.88
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$env:temp\$title.zip"
+Expand-Archive "$env:temp\$title.zip" "$env:SystemDrive\" -force
+
+#Configure default user
+set-content -path "$env:SystemDrive\$title\conf\tomcat-users.xml" "<?xml version='1.0' encoding='utf-8'?>`n<tomcat-users><role rolename='manager-gui'/>`n<user username='tomcat' password='s3cret' roles='manager-gui'/>`n</tomcat-users>"
+
+#Fix for current java version
+Set-Location "$location\bin"
+$serviceBatFix = Get-Content .\service.bat | Where-Object {$_ -notmatch '--JvmOptions9'}
+Set-Content .\service.bat $serviceBatFix
+
+#Install service
+Start-Process -Wait -FilePath "$env:SystemDrive\$title\bin\service.bat" -ArgumentList "install" -PassThru
+Set-Service Tomcat7 -StartupType Automatic
+Restart-Service Tomcat7 -Force
+
+#Allow traffic at firewall
+New-NetFirewallRule -DisplayName "Apache Tomcat 7 tcp 8080" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8080
+
+#Cleanup
+Remove-Item $env:temp\$title.zip
+
